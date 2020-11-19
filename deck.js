31a61ce5c6254d4472f068dac39758eb8771330a
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2020-11-19 20:20:05 +0100

    init

---
+++ deck.js
@@ -0,0 +1,36 @@
+const suits = ['♠', '♣ ', '♥', '♦']
+const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+export default class Deck {
+    constructor(cards = newDeck()) {
+        this.cards = cards;
+    }
+
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    shuffle() {
+        for (let i = this.numberOfCards - 1; i > 0; i--) {
+            const newIndex = Math.floor(Math.random() * (i + 1))
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldValue
+        }
+    }
+}
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
+
+function newDeck() {
+    return suits.flatMap(suit => {
+        return values.map(value => {
+            return new Card(suit, value);
+        })
+    })
+}
