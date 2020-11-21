0430fbdc1447f5f791e5055b06c03749d70864c7
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2020-11-21 18:43:10 +0100

    generate random cards and update cards left

--- deck.js
+++ deck.js
@@ -1,4 +1,4 @@
-const suits = ['♠', '♣ ', '♥', '♦']
+const suits = ['♠', '♣', '♥', '♦']
 const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 export default class Deck {
@@ -8,6 +8,14 @@ export default class Deck {
 
     get numberOfCards() {
         return this.cards.length
+    }
+
+    pop(){
+        return this.cards.shift();
+    }
+
+    push(card){
+        this.cards.push(card)
     }
 
     shuffle() {
@@ -24,6 +32,18 @@ class Card {
     constructor(suit, value) {
         this.suit = suit;
         this.value = value;
+    }
+
+    get color() {
+        return this.suit === '♠' || this.suit === '♣'  ? 'black' : 'red';
+    }
+
+    getHTML() {
+        const cardDiv = document.createElement('div');
+        cardDiv.innerText = this.value;
+        cardDiv.classList.add('card', this.color);
+        cardDiv.dataset.value = `${this.value} ${this.suit}`;
+        return cardDiv;
     }
 }
 
--- main.js
+++ main.js
@@ -1,5 +1,57 @@
 import Deck from './deck.js';
 
-const deck = new Deck();
-deck.shuffle()
-console.log(deck.cards)
+const cpuCardSlot = document.querySelector('.cpu-card-slot');
+const playerCardSlot = document.querySelector('.player-card-slot');
+const text = document.querySelector('.message')
+
+const cpuScore = document.querySelector('.cpu-counter');
+const playerScore = document.querySelector('.player-counter');
+
+
+let playerDeck;
+let cpuDeck;
+let inRound;
+
+document.addEventListener('click', () => {
+    if (inRound) {
+        roundRefresh();
+    }
+    else {
+        flipCards();
+    }
+})
+
+startGame();
+function startGame() {
+    const deck = new Deck();
+    deck.shuffle();
+    const deckHalf = Math.ceil(deck.numberOfCards / 2);
+    playerDeck = new Deck(deck.cards.slice(0, deckHalf));
+    cpuDeck = new Deck(deck.cards.slice(deckHalf, deck.numberOfCards));
+
+    roundRefresh()
+}
+
+function roundRefresh() {
+    inRound = false;
+    cpuCardSlot.innerHTML = '';
+    playerCardSlot.innerHTML = '';
+    text.innerText = '';
+    updateCounter();
+}
+
+function flipCards() {
+    inRound = true;
+    const playerCard = playerDeck.pop();
+    const cpuCard = cpuDeck.pop();
+    playerCardSlot.appendChild(playerCard.getHTML());
+    cpuCardSlot.appendChild(cpuCard.getHTML());
+
+}
+
+function updateCounter() {
+    cpuScore.innerText = cpuDeck.numberOfCards;
+    playerScore.innerText = playerDeck.numberOfCards;
+}
+
+
